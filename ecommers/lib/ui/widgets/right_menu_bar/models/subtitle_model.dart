ab929da473c232318d8465707aa34227f2e6a434
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2020-03-04 14:57:47 +0300

    added blur

--- ecommers/lib/ui/widgets/right_menu_bar/models/subtitle_model.dart
+++ ecommers/lib/ui/widgets/right_menu_bar/models/subtitle_model.dart
@@ -1,13 +1,7 @@
 import 'package:ecommers/ui/widgets/right_menu_bar/models/index.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class SubTitleModel extends RightMenuItemModel {
   final String subTitle;
 
   SubTitleModel({String title, this.subTitle}) : super(title: title);
-  @override
-  Widget create() {
-    return Text(subTitle);
-  }
 }
