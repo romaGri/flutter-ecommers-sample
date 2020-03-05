36afac7a8c3b422a56a938c6c2ccb85f6ec68ec2
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2020-03-05 16:40:15 +0300

    added clear button

--- ecommers/lib/ui/pages/search_page.dart
+++ ecommers/lib/ui/pages/search_page.dart
@@ -1,9 +1,8 @@
-import 'package:ecommers/ui/widgets/right_menu_bar/index.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RightMenuWidget();
+    return const Text('Search');
     }
 }
--- ecommers/lib/ui/widgets/right_menu_bar/right_menu_widget.dart
+++ ecommers/lib/ui/widgets/right_menu_bar/right_menu_widget.dart
@@ -48,74 +48,63 @@ class RightMenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      drawerScrimColor: Colors.transparent,
-      endDrawer: Container(
-        width: 270,
-        margin: EdgeInsets.zero,
-        child: Drawer(
-          elevation: 0.0,
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 5.0,
-              sigmaY: 5.0,
-            ),
-            child: Container(
-              color: BrandingColors.background,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          I18n.of(context).refineResults,
-                          style: Theme.of(context).textTheme.subtitle1.copyWith(
-                                fontWeight: FontWeight.w300,
-                                fontSize: FontSizes.small_3x,
-                              ),
-                        ),
-                        const Spacer(),
-                        Container(
-                          width: 45,
-                          child: FlatButton(
-                            padding: const EdgeInsets.all(1),
-                            materialTapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap,
-                            onPressed: () {},
-                            child: Text(
-                              I18n.of(context).clear,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .subtitle1
-                                  .copyWith(
-                                      fontSize: FontSizes.small_3x,
-                                      color: BrandingColors.primary),
+    return Container(
+      width: 270,
+      margin: EdgeInsets.zero,
+      child: Drawer(
+        elevation: 0.0,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 5.0,
+            sigmaY: 5.0,
+          ),
+          child: Container(
+            color: BrandingColors.background,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        I18n.of(context).refineResults,
+                        style: Theme.of(context).textTheme.subtitle1.copyWith(
+                              fontWeight: FontWeight.w300,
+                              fontSize: FontSizes.small_3x,
                             ),
-                          ),
+                      ),
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () => {},
+                        child: Text(
+                          I18n.of(context).clear,
+                          style: Theme.of(context).textTheme.subtitle1.copyWith(
+                              fontSize: FontSizes.small_3x,
+                              color: BrandingColors.primary),
                         ),
-                      ],
-                    ),
-                    RightMenuItemsList(
-                      itemList: filterMenuList,
-                    ),
-                    const SizedBox(height: 10.0),
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: SizedBox(
-                          height: 47.0,
-                          width: 230.0,
-                          child: PrimaryButtonWidget(
-                              text: I18n.of(context).applyFilters,
-                              onPressedFunction: () {}),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: Insets.x3_5),
+                  RightMenuItemsList(
+                    itemList: filterMenuList,
+                  ),
+                  const SizedBox(height: Insets.x2_5),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: SizedBox(
+                        height: 47.0,
+                        width: 230.0,
+                        child: PrimaryButtonWidget(
+                          text: I18n.of(context).applyFilters,
+                          onPressedFunction: () {},
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
